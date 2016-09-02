05d3b186142ffe301320073efe5d5ed443da3682
Meaghan Harty <mharty@localytics.com>
2016-09-02 15:53:54 -0400

    add wrapper methods and error class

--- cogito.py
+++ cogito.py
@@ -9,17 +9,30 @@ _mod = ctypes.cdll.LoadLibrary(_path)
 # 	  int status;
 # 	  char *message;
 # 	} response_t;
-# https://docs.python.org/2/library/ctypes.html#fundamental-data-types
 class ResponseT(ctypes.Structure):
   _fields_ = [('status', ctypes.c_int),
               ('message', ctypes.c_char_p),]
 
-# response_t cg_to_json()
-to_json = _mod.cg_to_json
-to_json.argtypes = (ctypes.c_char_p, )
-to_json.restype = ctypes.POINTER(ResponseT)
+# response_t cg_to_json
+cg_to_json = _mod.cg_to_json
+cg_to_json.argtypes = (ctypes.c_char_p, )
+cg_to_json.restype = ctypes.POINTER(ResponseT)
 
-# response_T
-to_iam = _mod.cg_to_iam
-to_iam.argtypes = (ctypes.c_char_p, )
-to_iam.restype = ctypes.POINTER(ResponseT)
+# response_t cg_to_iam
+cg_to_iam = _mod.cg_to_iam
+cg_to_iam.argtypes = (ctypes.c_char_p, )
+cg_to_iam.restype = ctypes.POINTER(ResponseT)
+
+def to_iam(args):
+  return check_status(cg_to_iam(args).contents)
+
+def to_json(args):
+  return check_status(cg_to_json(args).contents)
+
+def check_status(response):
+  if response.status != 0:
+    raise CogitoError(response.message)
+  return response.message
+
+class CogitoError(Exception):
+  pass
