82cba191e938c421654f22d312eaab4ba4af8105
Meaghan Harty <meag.harty@gmail.com>
2016-12-09 13:49:39 -0500

    Update cogito.py

--- cogito.py
+++ cogito.py
@@ -6,23 +6,25 @@ from ctypes.util import find_library
 _path = find_library('libcogito')
 _mod = ctypes.cdll.LoadLibrary(_path)
 
-# typedef struct response {
-# 	  int status;
-# 	  char *message;
-# 	} response_t;
-class ResponseT(ctypes.Structure):
-    _fields_ = [('status', ctypes.c_int),
-                ('message', ctypes.c_char_p), ]
+# typedef struct cg_buf {
+#   size_t length;
+#   size_t capacity;
+#   char *content;
+# } cg_buf_t;
+class CgBuf(ctypes.Structure):
+    _fields_ = [('length', ctypes.c_size_t),
+                ('capacity', ctypes.c_size_t),
+                ('content', ctypes.c_char_p), ]
 
 # response_t cg_to_json
 cg_to_json = _mod.cg_to_json
-cg_to_json.argtypes = (ctypes.c_char_p, )
-cg_to_json.restype = ctypes.POINTER(ResponseT)
+cg_to_json.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p, )
+cg_to_json.restype = ctypes.c_int
 
 # response_t cg_to_iam
 cg_to_iam = _mod.cg_to_iam
-cg_to_iam.argtypes = (ctypes.c_char_p, )
-cg_to_iam.restype = ctypes.POINTER(ResponseT)
+cg_to_iam.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p, )
+cg_to_iam.restype = ctypes.c_int
 
 
 def to_iam(args):
@@ -34,9 +36,9 @@ def to_json(args):
 
 
 def check_status(response):
-    if response.status != 0:
+    if response != 0:
         raise CogitoError(response.message)
-    return response.message
+    return response
 
 
 class CogitoError(Exception):
