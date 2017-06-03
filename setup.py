0301a11145ae4e0d61143b97a6c6d03f530a540b
Kevin Deisz <kevin.deisz@gmail.com>
2017-06-02 22:11:47 -0400

    Bump libcogito version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cogito',
-    version='0.0.1',
+    version='0.2.0',
     py_modules=['cogito'],
     description='A tiny python library that links against libcogito',
     url='https://github.com/localytics/cogito-py',
