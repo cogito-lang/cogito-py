b91ace29244c12e8ad9df129db41dbe845d26a0c
Kevin Deisz <kevin.deisz@gmail.com>
2019-05-28 17:52:22 -0400

    Start linting with pylint

--- cogito.py
+++ cogito.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+"""A tiny python library that links against libcogito"""
 
 import os
 import ctypes
@@ -26,11 +26,11 @@ if _path is None:
 
     raise NameError(message)
 
-_mod = ctypes.cdll.LoadLibrary(_path)
-
 
 class CgBuf(ctypes.Structure):
     """
+    A ctypes structure that wraps the cg_buf struct from libcogito
+
     typedef struct cg_buf {
       size_t length;
       size_t capacity;
@@ -41,34 +41,33 @@ class CgBuf(ctypes.Structure):
                 ('capacity', ctypes.c_size_t),
                 ('content', ctypes.c_char_p), ]
 
+
+COGITO = ctypes.cdll.LoadLibrary(_path)
+
 # int cg_to_json
-cg_to_json = _mod.cg_to_json
-cg_to_json.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p)
-cg_to_json.restype = ctypes.c_int
+COGITO.cg_to_json.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p)
+COGITO.cg_to_json.restype = ctypes.c_int
 
 # int cg_to_iam
-cg_to_iam = _mod.cg_to_iam
-cg_to_iam.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p)
-cg_to_iam.restype = ctypes.c_int
+COGITO.cg_to_iam.argtypes = (ctypes.POINTER(CgBuf), ctypes.c_char_p)
+COGITO.cg_to_iam.restype = ctypes.c_int
 
 # cg_buf_t* cg_buf_build(void);
-cg_buf_build = _mod.cg_buf_build
-cg_buf_build.argtypes = None
-cg_buf_build.restype = ctypes.POINTER(CgBuf)
+COGITO.cg_buf_build.argtypes = None
+COGITO.cg_buf_build.restype = ctypes.POINTER(CgBuf)
 
 # void cg_buf_free(cg_buf_t *buffer);
-cg_buf_free = _mod.cg_buf_free
-cg_buf_free.argtypes = (ctypes.POINTER(CgBuf), )
-cg_buf_free.restype = None
+COGITO.cg_buf_free.argtypes = (ctypes.POINTER(CgBuf), )
+COGITO.cg_buf_free.restype = None
 
 
 def to_iam(args):
-    buf = cg_buf_build()
-    if cg_to_iam(buf, ctypes.c_char_p(args.encode("utf-8"))) != 0:
+    buf = COGITO.cg_buf_build()
+    if COGITO.cg_to_iam(buf, ctypes.c_char_p(args.encode("utf-8"))) != 0:
         raise CogitoError("IAM conversion failed")
 
     response = buf.contents.content.decode("utf-8")
-    cg_buf_free(buf)
+    COGITO.cg_buf_free(buf)
     return response
 
 
@@ -76,12 +75,12 @@ def to_json(args, subs=None):
     for key, value in (subs or {}).items():
         args = args.replace("${{{}}}".format(key), value)
 
-    buf = cg_buf_build()
-    if cg_to_json(buf, ctypes.c_char_p(args.encode("utf-8"))) != 0:
+    buf = COGITO.cg_buf_build()
+    if COGITO.cg_to_json(buf, ctypes.c_char_p(args.encode("utf-8"))) != 0:
         raise CogitoError("JSON conversion failed")
 
     response = buf.contents.content.decode("utf-8")
-    cg_buf_free(buf)
+    COGITO.cg_buf_free(buf)
     return response
 
 
