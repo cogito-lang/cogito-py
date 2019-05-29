e5d0a00d15386f6dee6a563b1193ea21aecb5373
Kevin Deisz <kevin.deisz@gmail.com>
2019-05-29 07:58:49 -0400

    Documentation everywhere

--- cogito.py
+++ cogito.py
@@ -10,33 +10,22 @@ else:
     COGITO_PATH = os.environ["COGITO_PATH"]
 
 if COGITO_PATH is None:
-    message = "libcogito is missing from your system. " \
-        "Please install by running the following steps:\n"
-
-    if os.popen("uname").read().strip() == "Darwin":
-        message += """
-    $ brew tap cogito-lang/formulae
-    $ brew install cogito
-"""
-    else:
-        message += """
-    $ FILE=$(mktemp)
-    $ wget 'https://github.com/cogito-lang/libcogito/releases/download/v0.2.0/libcogito_0.2.0-1_amd64.deb' -qO $FILE
-    $ sudo dpkg -i $FILE && rm $FILE
-"""
-
-    raise NameError(message)
+    raise NameError(
+        "libcogito is missing from your system. " \
+        "Please go to https://github.com/cogito-lang/libcogito " \
+        "and follow the installation instructions."
+    )
 
 
 class CogitoBuffer(ctypes.Structure):
     """
     A ctypes structure that wraps the cg_buf struct from libcogito
 
-    typedef struct cg_buf {
+    struct cg_buf {
       size_t length;
       size_t capacity;
       char *content;
-    } cg_buf_t;
+    };
     """
 
     _fields_ = [
@@ -47,28 +36,46 @@ class CogitoBuffer(ctypes.Structure):
 
 
 class CogitoError(Exception):
-    pass
+    """A generic error to represent when the C library conversion fails"""
 
 
 class Cogito:
+    """A context manager used for allocating and releasing a cogito buffer"""
+
     def __init__(self):
         self.buffer = None
 
     def __enter__(self):
+        """Allocates and stores a cogito buffer"""
+
         self.buffer = COGITO.cg_buf_build()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Releases the stored cogito buffer"""
+
         COGITO.cg_buf_free(self.buffer)
 
     def to_iam(self, content):
-        if COGITO.cg_to_iam(self.buffer, ctypes.c_char_p(content.encode("utf-8"))) != 0:
+        """
+        Calls the native cg_to_iam C function and returns the result that
+        is now stored in the buffer struct
+        """
+
+        c_char_p = ctypes.c_char_p(content.encode("utf-8"))
+        if COGITO.cg_to_iam(self.buffer, c_char_p) != 0:
             raise CogitoError("IAM conversion failed")
 
         return self.buffer.contents.content.decode("utf-8")
 
     def to_json(self, content):
-        if COGITO.cg_to_json(self.buffer, ctypes.c_char_p(content.encode("utf-8"))) != 0:
+        """
+        Calls the native cg_to_json C function and returns the result that
+        is now stored in the buffer struct
+        """
+
+        c_char_p = ctypes.c_char_p(content.encode("utf-8"))
+        if COGITO.cg_to_json(self.buffer, c_char_p) != 0:
             raise CogitoError("JSON conversion failed")
 
         return self.buffer.contents.content.decode("utf-8")
@@ -94,6 +101,8 @@ COGITO.cg_buf_free.restype = None
 
 
 def to_iam(content):
+    """Converts the given JSON string into IAM syntax"""
+
     response = None
     with Cogito() as cogito:
         response = cogito.to_iam(content)
@@ -102,6 +111,8 @@ def to_iam(content):
 
 
 def to_json(content, substitutions=None):
+    """Converts the given IAM string into JSON syntax"""
+
     for key, value in (substitutions or {}).items():
         content = content.replace("${{{}}}".format(key), value)
 
